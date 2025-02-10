ce4fe6a3c4141e83d2a932d9cd5046754e2b028c
unknown <paul.menard@imt-atlantique.fr>
2025-02-10 12:46:29 -0500

    Mise a jour partie 6

--- Birthday/app/src/main/java/com/example/birthday/MainActivity.kt
+++ Birthday/app/src/main/java/com/example/birthday/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.birthday.ui.theme.BirthdayTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +36,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message : String, modifier: Modifier = Modifier) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
     )
 }
 
